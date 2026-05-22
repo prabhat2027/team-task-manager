3a7f3de7bf9b65446bdda0b5cb5236a02a8445ea
PV <work.prabhat2700@gmail.com>
2026-05-22 21:54:26 +0530

    fix volume mount path

--- src/db/database.js
+++ src/db/database.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 const dbPath = process.env.NODE_ENV === 'production'
-  ? '/app/database.db'
+  ? '/app/data/database.db'
   : 'database.db';
 const db = new Database(dbPath);
 
